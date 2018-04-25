1ef62c9446981ebd94e7f8de75260090fd8dfbb3
yunfei <mayunfei6@gmail.com>
2018-04-25 19:00:31 +0800

    添加登录注册

--- AndroidDemo/app/src/main/java/com/demo/kaiyun/androiddemo/bean/Student.java
+++ AndroidDemo/app/src/main/java/com/demo/kaiyun/androiddemo/bean/Student.java
@@ -20,6 +20,26 @@ public class Student {
 
     private String andress;
 
+    private String number;
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
     /**
      * @return id
      */
